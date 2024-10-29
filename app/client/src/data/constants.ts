36c151950580dfa48229481cb62f0737fabc9864
Hyungho Seo <hyungho.seo@jpmchase.com>
2024-10-29 16:38:02 -0400

    update github url

--- app/client/src/data/constants.ts
+++ app/client/src/data/constants.ts
@@ -10,7 +10,7 @@ export const PLATFORM_ID = import.meta.env.PLATFORM_ID || '0000123456';
 export const CLIENT_TOKEN = import.meta.env.CLIENT_TOKEN || 'fake-token';
 
 export const GITHUB_REPO =
-  'https://github.com/jpmorgan-payments/embedded-banking';
+  'https://github.com/jpmorgan-payments/embedded-finance';
 
 export const FORGEROCK_CONFIG = {
   AM_URL: `https://${import.meta.env.VITE_FR_URL}/am`,
