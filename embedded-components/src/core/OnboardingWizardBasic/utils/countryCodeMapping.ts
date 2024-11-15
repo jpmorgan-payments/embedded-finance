91d699da79b30959c464d69e80085058f19d6be3
Hyungho Seo <hyungho.seo@jpmchase.com>
2024-11-15 14:12:19 -0500

    update country code mapping

--- embedded-components/src/core/OnboardingWizardBasic/utils/countryCodeMapping.ts
+++ embedded-components/src/core/OnboardingWizardBasic/utils/countryCodeMapping.ts
@@ -1,6 +1,6 @@
-import { Jurisdiction } from './types';
+import { CountryCodeIsoAlpha2 } from '@/api/generated/smbdo.schemas';
 
-export const COUNTRY_CODE_MAPPING: Record<Jurisdiction, string> = {
+export const COUNTRY_CODE_MAPPING: Record<CountryCodeIsoAlpha2, string> = {
   CA: 'Canada',
   US: 'United States',
 };
