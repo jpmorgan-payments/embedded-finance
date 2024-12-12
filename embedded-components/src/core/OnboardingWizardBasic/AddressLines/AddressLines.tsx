251fdc5006b48a315498b3da19ad04a78af41980
Hyungho Seo <hyungho.seo@jpmchase.com>
2024-12-12 10:47:21 -0500

    Move AddressLines to separate component

---
+++ embedded-components/src/core/OnboardingWizardBasic/AddressLines/AddressLines.tsx
@@ -0,0 +1,101 @@
+import { FC } from 'react';
+import { useFieldArray } from 'react-hook-form';
+
+import {
+  Button,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+} from '@/components/ui';
+import { InfoPopover } from '@/components/ux/InfoPopover';
+
+type AddressLinesProps = {
+  control: any;
+  addressIndex: number;
+};
+
+export const AddressLines: FC<AddressLinesProps> = ({
+  control,
+  addressIndex,
+}) => {
+  const {
+    fields: addressLineFields,
+    append: appendAddressLine,
+    remove: removeAddressLine,
+  } = useFieldArray({
+    control,
+    name: `addresses.${addressIndex}.addressLines`,
+    rules: {
+      maxLength: 5,
+      minLength: 1,
+    },
+  });
+
+  return (
+    <>
+      <FormField
+        control={control}
+        name={`addresses.${addressIndex}.addressLines.0`}
+        render={({ field }) => (
+          <FormItem>
+            <div className="eb-flex eb-items-center eb-space-x-2">
+              <FormLabel asterisk>Address Line 1</FormLabel>
+              <InfoPopover>
+                The first line must not be a PO Box and must begin with a
+                number.
+              </InfoPopover>
+            </div>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      {addressLineFields.map((addressLineField, index) => {
+        if (index === 0) return null;
+        return (
+          <FormField
+            key={addressLineField.id}
+            control={control}
+            name={`addresses.${addressIndex}.addressLines.${index}`}
+            render={({ field }) => (
+              <FormItem>
+                <div className="eb-flex eb-items-center eb-space-x-2">
+                  <FormLabel>Address Line {index + 1}</FormLabel>
+                </div>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        );
+      })}
+      <div className="eb-grid eb-grid-cols-2 eb-gap-6">
+        <Button
+          type="button"
+          variant="outline"
+          size="sm"
+          onClick={() => appendAddressLine('')}
+          disabled={addressLineFields.length >= 5}
+        >
+          Add Line
+        </Button>
+        <Button
+          type="button"
+          variant="outline"
+          size="sm"
+          onClick={() => removeAddressLine(addressLineFields.length - 1)}
+          disabled={addressLineFields.length <= 1}
+        >
+          Remove Line
+        </Button>
+      </div>
+    </>
+  );
+};
--- embedded-components/src/core/OnboardingWizardBasic/OrganizationStepForm/OrganizationStepForm.tsx
+++ embedded-components/src/core/OnboardingWizardBasic/OrganizationStepForm/OrganizationStepForm.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import { FC, Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { CaretSortIcon, CheckIcon } from '@radix-ui/react-icons';
 import { useFieldArray, useForm } from 'react-hook-form';
@@ -46,6 +46,7 @@ import { useStepper } from '@/components/ui/stepper';
 import { RadioGroup, RadioGroupItem } from '@/components/ui';
 import { InfoPopover } from '@/components/ux/InfoPopover';
 
+import { AddressLines } from '../AddressLines/AddressLines';
 import { FormActions } from '../FormActions/FormActions';
 import { FormLoadingState } from '../FormLoadingState/FormLoadingState';
 import { useOnboardingContext } from '../OnboardingContextProvider/OnboardingContextProvider';
@@ -63,91 +64,6 @@ import {
   OrganizationStepFormSchema,
   refineOrganizationStepFormSchema,
 } from './OrganizationStepForm.schema';
-
-type AddressLinesProps = {
-  control: any;
-  addressIndex: number;
-};
-
-const AddressLines: FC<AddressLinesProps> = ({ control, addressIndex }) => {
-  const {
-    fields: addressLineFields,
-    append: appendAddressLine,
-    remove: removeAddressLine,
-  } = useFieldArray({
-    control,
-    name: `addresses.${addressIndex}.addressLines`,
-    rules: {
-      maxLength: 5,
-      minLength: 1,
-    },
-  });
-
-  return (
-    <>
-      <FormField
-        control={control}
-        name={`addresses.${addressIndex}.addressLines.0`}
-        render={({ field }) => (
-          <FormItem>
-            <div className="eb-flex eb-items-center eb-space-x-2">
-              <FormLabel asterisk>Address Line 1</FormLabel>
-              <InfoPopover>
-                The first line must not be a PO Box and must begin with a
-                number.
-              </InfoPopover>
-            </div>
-            <FormControl>
-              <Input {...field} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      {addressLineFields.map((addressLineField, index) => {
-        if (index === 0) return null;
-        return (
-          <FormField
-            key={addressLineField.id}
-            control={control}
-            name={`addresses.${addressIndex}.addressLines.${index}`}
-            render={({ field }) => (
-              <FormItem>
-                <div className="eb-flex eb-items-center eb-space-x-2">
-                  <FormLabel>Address Line {index + 1}</FormLabel>
-                </div>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        );
-      })}
-      <div className="eb-grid eb-grid-cols-2 eb-gap-6">
-        <Button
-          type="button"
-          variant="outline"
-          size="sm"
-          onClick={() => appendAddressLine('')}
-          disabled={addressLineFields.length >= 5}
-        >
-          Add Line
-        </Button>
-        <Button
-          type="button"
-          variant="outline"
-          size="sm"
-          onClick={() => removeAddressLine(addressLineFields.length - 1)}
-          disabled={addressLineFields.length <= 1}
-        >
-          Remove Line
-        </Button>
-      </div>
-    </>
-  );
-};
 
 export const OrganizationStepForm = () => {
   const { nextStep } = useStepper();
