8bb54cdeb81b5d9456984e60d0c3fcb82f69c9ca
Yevgen Kontelov <491908+kontel@users.noreply.github.com>
2025-01-03 16:06:16 -0500

    Remove unused OnboardingWizard components and utilities

--- embedded-components/src/core/OnboardingWizard/WizardSteps/yup/businessOwnersValidation.schema.ts
+++
@@ -1,9 +0,0 @@
-// import * as yup from 'yup';
-
-export const businessOwnersValidation: any = () => {
-  return {
-    // entitiesInOwnership: yup
-    //   .boolean()
-    //   .required(getContentToken ?? 'Required field'),
-  };
-};
--- embedded-components/src/index.tsx
+++ embedded-components/src/index.tsx
@@ -1,11 +1,9 @@
 import './index.css';
 
 export { LinkedAccountWidget } from './core/LinkedAccountWidget/LinkedAccountWidget';
-export { OnboardingWizard } from './core/OnboardingWizard/OnboardingWizard';
 
 export * from './core/EBComponentsProvider';
 
-export { StepperProvider } from './core/OnboardingWizard/Stepper/Stepper';
 export { initEBComponentsManager } from './vanilla/EBComponentsManager';
 
 export * from './core/TransactionsDisplay/TransactionsDisplay';
