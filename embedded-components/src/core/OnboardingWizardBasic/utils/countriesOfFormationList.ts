acc51450572c97a4a179e07082be96b157666c95
Yevgen Kontelov <491908+kontel@users.noreply.github.com>
2025-01-08 16:29:35 -0500

    prettier

--- embedded-components/src/core/OnboardingWizardBasic/utils/countriesOfFormationList.ts
+++ embedded-components/src/core/OnboardingWizardBasic/utils/countriesOfFormationList.ts
@@ -1,5 +1,24 @@
 import { CountryCodeIsoAlpha2 } from '@/api/generated/smbdo.schemas';
 
 export const COUNTRIES_OF_FORMATION: CountryCodeIsoAlpha2[] = [
-'US', 'CA', 'GB', 'FR', 'DE', 'JP', 'CN', 'IN', 'BR', 'RU', 'ZA', 'AU', 'IT', 'ES', 'MX', 'KR', 'NL', 'TR', 'SA', 'CH'
+  'US',
+  'CA',
+  'GB',
+  'FR',
+  'DE',
+  'JP',
+  'CN',
+  'IN',
+  'BR',
+  'RU',
+  'ZA',
+  'AU',
+  'IT',
+  'ES',
+  'MX',
+  'KR',
+  'NL',
+  'TR',
+  'SA',
+  'CH',
 ];
