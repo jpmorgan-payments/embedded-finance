5c05d4e0c7bbea0b03d900ab8a6f486538754382
Yevgen Kontelov <491908+kontel@users.noreply.github.com>
2025-01-08 12:10:03 -0500

    Refactor import statement to use smbdo.schemas in efClientCorpAnsweredQuestions.mock.ts

--- embedded-components/src/mocks/efClientCorpAnsweredQuestions.mock.ts
+++ embedded-components/src/mocks/efClientCorpAnsweredQuestions.mock.ts
@@ -1,4 +1,4 @@
-import { ClientResponse } from '../api/generated/embedded-banking.schemas';
+import { ClientResponse } from "@/api/generated/smbdo.schemas";
 
 export const efClientCorpAnsweredQuestions: ClientResponse = {
   id: '0030000130',
