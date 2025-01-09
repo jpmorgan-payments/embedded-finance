7cba6c971207261c7b888a499d638601c9dffdb8
Yevgen Kontelov <491908+kontel@users.noreply.github.com>
2025-01-09 16:18:55 -0500

    prettier

--- embedded-components/src/core/OnboardingWizardBasic/DocumentUploadStepForm/DocumentUploadStepForm.schema.ts
+++ embedded-components/src/core/OnboardingWizardBasic/DocumentUploadStepForm/DocumentUploadStepForm.schema.ts
@@ -1,10 +1,9 @@
 import { z } from 'zod';
 
 export const DocumentUploadStepFormSchema = z.object({
-  file: z.instanceof(Array<File>).refine(files => 
-    files.every(file => file.size < 2 * 1024 * 1024), 
-    {
-      message: "Each file must be less than 2MB",
-    }
-  ),
+  file: z
+    .instanceof(Array<File>)
+    .refine((files) => files.every((file) => file.size < 2 * 1024 * 1024), {
+      message: 'Each file must be less than 2MB',
+    }),
 });
