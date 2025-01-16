22498e83978efb88260de6bde1aa22ea00f2767b
Eugene Kontelev <491908+kontel@users.noreply.github.com>
2025-01-15 19:39:25 -0500

    Refactor Textarea component for consistency and code style

--- embedded-components/src/components/ui/textarea.tsx
+++ embedded-components/src/components/ui/textarea.tsx
@@ -1,22 +1,22 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
-  React.ComponentProps<"textarea">
+  React.ComponentProps<'textarea'>
 >(({ className, ...props }, ref) => {
   return (
     <textarea
       className={cn(
-        "eb-flex eb-min-h-[80px] eb-w-full eb-rounded-md eb-border eb-border-input eb-bg-background eb-px-3 eb-py-2 eb-text-base eb-ring-offset-background placeholder:eb-text-muted-foreground focus-visible:eb-outline-none focus-visible:eb-ring-2 focus-visible:eb-ring-ring focus-visible:eb-ring-offset-2 disabled:eb-cursor-not-allowed disabled:eb-opacity-50 md:eb-text-sm",
+        'eb-flex eb-min-h-[80px] eb-w-full eb-rounded-md eb-border eb-border-input eb-bg-background eb-px-3 eb-py-2 eb-text-base eb-ring-offset-background placeholder:eb-text-muted-foreground focus-visible:eb-outline-none focus-visible:eb-ring-2 focus-visible:eb-ring-ring focus-visible:eb-ring-offset-2 disabled:eb-cursor-not-allowed disabled:eb-opacity-50 md:eb-text-sm',
         className
       )}
       ref={ref}
       {...props}
     />
-  )
-})
-Textarea.displayName = "Textarea"
+  );
+});
+Textarea.displayName = 'Textarea';
 
-export { Textarea }
+export { Textarea };
