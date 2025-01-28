258afa9c6dff1fcf042e8f6f633b954dc9f1ffd0
Hyungho Seo <hyungho.seo@jpmchase.com>
2025-01-28 12:55:31 -0500

    localize button labels in FormActions component

--- embedded-components/src/core/OnboardingWizardBasic/FormActions/FormActions.tsx
+++ embedded-components/src/core/OnboardingWizardBasic/FormActions/FormActions.tsx
@@ -1,15 +1,18 @@
+import { useTranslation } from 'react-i18next';
+
 import { useStepper } from '@/components/ui/stepper';
 import { Button } from '@/components/ui';
 
 export const FormActions = () => {
   const { prevStep, isDisabledStep, isLastStep } = useStepper();
+  const { t } = useTranslation('common');
 
   return (
     <div className="eb-flex eb-w-full eb-justify-end eb-gap-4">
       <Button disabled={isDisabledStep} variant="secondary" onClick={prevStep}>
-        Previous
+        {t('previous')}
       </Button>
-      <Button>{isLastStep ? 'Submit' : 'Next'}</Button>
+      <Button>{isLastStep ? t('submit') : t('next')}</Button>
     </div>
   );
 };
