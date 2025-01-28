bc31fe1c5c558cbd7cfc76013882c96ea2a841aa
Eugene Kontelev <491908+kontel@users.noreply.github.com>
2025-01-28 16:34:18 -0500

    Update onboarding scenarios, mocks, and client configuration

--- app/client/src/mocks/index.ts
+++ app/client/src/mocks/index.ts
@@ -12,3 +12,5 @@ export { recipientsMock } from './recipients.mock';
 export { transactionCreateResponseMock } from './transactionCreateResponse.mock';
 export { transactionDetailsMock } from './transactionDetails.mock';
 export { transactionsMock } from './transactions.mock';
+export { efClientQuestionsMock } from './efClientQuestions.mock';
+export { efDocumentClientDetail } from './efDocumentClientDetail.mock';
