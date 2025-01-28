42e0ebf3ade9badd291f0d5f4467f2e3ec94407d
Eugene Kontelev <491908+kontel@users.noreply.github.com>
2025-01-28 13:27:04 -0500

    Minor formatting adjustment in EBComponentsProvider component

--- embedded-components/src/core/EBComponentsProvider/EBComponentsProvider.tsx
+++ embedded-components/src/core/EBComponentsProvider/EBComponentsProvider.tsx
@@ -1,20 +1,24 @@
-import { createContext, lazy, PropsWithChildren, useContext, useEffect, useMemo, useState, type ErrorInfo } from 'react';
+import {
+  createContext,
+  lazy,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+  type ErrorInfo,
+} from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AlertCircle } from 'lucide-react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useTranslation } from 'react-i18next';
 
-
-
 import { loadContentTokens } from '@/lib/utils';
 import { AXIOS_INSTANCE } from '@/api/axios-instance';
 import { Toaster } from '@/components/ui/sonner';
 
-
-
 import { EBConfig } from './config.types';
 import { convertThemeToCssString } from './convert-theme-to-css-variables';
-
 
 const queryClient = new QueryClient();
 
