37453431c842a3ef10324471185fc1b84835f99e
Eugene Kontelev <491908+kontel@users.noreply.github.com>
2025-02-05 09:48:21 -0500

    Refactor PatternMaskInput component with dynamic format and improved masking

--- embedded-components/src/components/ux/PatternMaskInput.tsx
+++ embedded-components/src/components/ux/PatternMaskInput.tsx
@@ -1,4 +1,4 @@
-import React, { FocusEventHandler, useState } from 'react';
+import React, { useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
@@ -9,21 +9,23 @@ import {
   InputOTPSlot,
 } from '@/components/ui/input-otp';
 
+type WebKitStyle = React.CSSProperties & {
+  WebkitTextSecurity?: 'disc' | 'circle' | 'square' | 'none';
+};
+
 interface PatternMaskInputProps {
-  maxLength: number;
   masked?: boolean;
   pattern?: string;
-  separator?: string | React.ReactNode;
+  format: string;
   value: string;
   onChange: (value: string) => void;
-  onBlur?: FocusEventHandler<HTMLInputElement>;
+  onBlur?: () => void;
 }
 
 export function PatternMaskInput({
-  maxLength,
   masked = false,
   pattern,
-  separator,
+  format,
   value,
   onChange,
   onBlur,
@@ -36,30 +38,38 @@ export function PatternMaskInput({
 
   return (
     <div className="eb-space-y-2">
-      <div className="eb-flex eb-items-center eb-space-x-2">
+      <div className="eb-flex eb-items-center eb-space-x-1">
         <InputOTP
           value={value}
           onChange={onChange}
           onBlur={onBlur}
-          maxLength={maxLength}
+          maxLength={format.replace(/[^#]/g, '').length}
           pattern={pattern}
         >
           <InputOTPGroup>
-            {Array.from({ length: maxLength }).map((_, index) => (
-              <React.Fragment key={index}>
-                {index > 0 && index % 3 === 0 && (
-                  <InputOTPSeparator>{separator}</InputOTPSeparator>
-                )}
-                <InputOTPSlot
-                  index={index}
-                  style={
-                    showMasked
-                      ? ({ WebkitTextSecurity: 'disc' } as any)
-                      : undefined
-                  }
-                />
-              </React.Fragment>
-            ))}
+            {format.split('').map((char, index) => {
+              return (
+                <React.Fragment key={index}>
+                  {char === '#' ? (
+                    <InputOTPSlot
+                      index={
+                        index -
+                        ((format?.slice(0, index)?.match(/[^#]/g) ?? [])
+                          .length ?? 0)
+                      }
+                      style={
+                        showMasked
+                          ? ({ WebkitTextSecurity: 'disc' } as WebKitStyle)
+                          : {}
+                      }
+                      className="eb-h-8 eb-w-8"
+                    />
+                  ) : (
+                    <InputOTPSeparator>{char}</InputOTPSeparator>
+                  )}
+                </React.Fragment>
+              );
+            })}
           </InputOTPGroup>
         </InputOTP>
         {masked && (
@@ -71,9 +81,9 @@ export function PatternMaskInput({
             onMouseLeave={() => setShowMasked(true)}
           >
             {showMasked ? (
-              <Eye className="eb-h-4 eb-w-4" />
-            ) : (
               <EyeOff className="eb-h-4 eb-w-4" />
+            ) : (
+              <Eye className="eb-h-4 eb-w-4" />
             )}
           </Button>
         )}
