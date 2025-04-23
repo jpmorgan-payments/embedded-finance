ccc196bb42ce72e90621c9546aca0b3fc84af9e0
Hyungho Seo <hyungho.seo@jpmchase.com>
2025-04-23 15:16:39 -0400

    add business description tooltip

--- embedded-components/src/core/OnboardingWizardBasic/OnboardingOverviewFlow/OnboardingSectionStepper/BusinessSectionForms/CustomerFacingDetailsForm/CustomerFacingDetailsForm.tsx
+++ embedded-components/src/core/OnboardingWizardBasic/OnboardingOverviewFlow/OnboardingSectionStepper/BusinessSectionForms/CustomerFacingDetailsForm/CustomerFacingDetailsForm.tsx
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
+import { InfoIcon } from 'lucide-react';
 import { useFormContext } from 'react-hook-form';
 // import { useTranslation } from 'react-i18next';
 import { z } from 'zod';
 
+import { Button } from '@/components/ui';
 import { OnboardingFormField } from '@/core/OnboardingWizardBasic/OnboardingFormField/OnboardingFormField';
 
+import { LearnMorePopoverTrigger } from '../../../LearnMorePopover/LearnMorePopover';
 import { SectionStepFormComponent } from '../../../types';
 import {
   CustomerFacingDetailsFormSchema,
@@ -62,11 +65,50 @@ export const CustomerFacingDetailsForm: SectionStepFormComponent = ({
           noOptionalLabel
         />
       </div>
-      <OnboardingFormField
-        control={form.control}
-        name="organizationDescription"
-        type="textarea"
-      />
+      <div>
+        <OnboardingFormField
+          control={form.control}
+          name="organizationDescription"
+          type="textarea"
+        />
+        <LearnMorePopoverTrigger
+          content={
+            <div className="eb-space-y-3">
+              <h2 className="eb-font-header eb-text-xl eb-font-medium">
+                What is a business description?
+              </h2>
+              <p className="eb-text-sm">
+                It&apos;s a snapshot of your business. A summary of your
+                company&apos;s operations, including the industry, the nature of
+                the business activities, and the specific products you offer,
+                including your top selling items.
+              </p>
+              <p className="eb-pb-1 eb-text-sm">
+                Please ensure this aligns with the business classification you
+                have selected.
+              </p>
+              <h2 className="eb-font-header eb-text-xl eb-font-medium">
+                Will this be shown to customers?
+              </h2>
+              <p className="eb-pb-1 eb-text-sm">
+                No, this is only for this application.
+              </p>
+              <h2 className="eb-font-header eb-text-xl eb-font-medium">
+                Example
+              </h2>
+              <p className="eb-pb-1 eb-text-sm">
+                {
+                  '{Business name} creates sustainable and affordable beauty and hair care products that are vegan and cruelty free. Our top selling products are our organic citrus shampoo and conditioner line as well as our paraben and sulfate free mascara.'
+                }
+              </p>
+            </div>
+          }
+        >
+          <Button className="eb-text-primary" size="sm" variant="ghost">
+            Learn more <InfoIcon />
+          </Button>
+        </LearnMorePopoverTrigger>
+      </div>
       <div className="eb-space-y-2">
         <OnboardingFormField
           control={form.control}
