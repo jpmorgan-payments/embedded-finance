9ad11d423337d3a14945df92aecab703c093013b
Hyungho Seo <hyungho.seo@jpmchase.com>
2025-04-23 10:38:34 -0400

    add sample LearnMore

--- embedded-components/src/core/OnboardingWizardBasic/OnboardingOverviewFlow/OnboardingSectionStepper/BusinessSectionForms/IndustryForm/IndustryForm.tsx
+++ embedded-components/src/core/OnboardingWizardBasic/OnboardingOverviewFlow/OnboardingSectionStepper/BusinessSectionForms/IndustryForm/IndustryForm.tsx
@@ -1,9 +1,12 @@
+import { InfoIcon } from 'lucide-react';
 import { useFormContext } from 'react-hook-form';
 // import { useTranslation } from 'react-i18next';
 import { z } from 'zod';
 
+import { Button } from '@/components/ui';
 import { OnboardingFormField } from '@/core/OnboardingWizardBasic/OnboardingFormField/OnboardingFormField';
 
+import { LearnMorePopoverTrigger } from '../../../LearnMorePopover/LearnMorePopover';
 import { SectionStepFormComponent } from '../../../types';
 import { IndustryFormSchema } from './IndustryForm.schema';
 
@@ -13,6 +16,11 @@ export const IndustryForm: SectionStepFormComponent = () => {
 
   return (
     <div className="eb-space-y-6">
+      <LearnMorePopoverTrigger content={<div>hello</div>}>
+        <Button className="-eb-mt-6 eb-text-primary" variant="ghost">
+          Learn more <InfoIcon />
+        </Button>
+      </LearnMorePopoverTrigger>
       <OnboardingFormField
         control={form.control}
         name="industry"
