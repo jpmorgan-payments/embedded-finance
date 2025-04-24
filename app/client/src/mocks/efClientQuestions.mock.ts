14cf257a1c7aa9e86329350e9e4b43c86f98c641
Eugene Kontelev <491908+kontel@users.noreply.github.com>
2025-04-24 11:19:05 -0400

    feat: add new client questions and update mock data for enhanced compliance checks

--- app/client/src/mocks/efClientQuestions.mock.ts
+++ app/client/src/mocks/efClientQuestions.mock.ts
@@ -20,5 +20,57 @@ export const efClientQuestionsMock = {
       },
       subQuestions: [],
     },
+
+    {
+      content: [
+        {
+          description:
+            'Do you have locations, sell goods, or services, or have vendors or suppliers in countries or regions subject to comprehensive sanctions programs (Iran, North Korea, Cuba, Syria and the Crimea, Donetsk, Luhansk Regions of Ukraine), or work with Sanctioned Parties in Russia or Venezuela?',
+          label:
+            'Do you have locations, sell goods, or services, or have vendors or suppliers in countries or regions subject to comprehensive sanctions programs (Iran, North Korea, Cuba, Syria and the Crimea, Donetsk, Luhansk Regions of Ukraine), or work with Sanctioned Parties in Russia or Venezuela?',
+          locale: 'en-US',
+        },
+      ],
+      defaultLocale: 'en-US',
+      description:
+        'Do you have locations, sell goods, or services, or have vendors or suppliers in countries or regions subject to comprehensive sanctions programs (Iran, North Korea, Cuba, Syria and the Crimea, Donetsk, Luhansk Regions of Ukraine), or work with Sanctioned Parties in Russia or Venezuela?',
+      id: '300006',
+      responseSchema: {
+        type: 'array',
+        minItems: 1,
+        maxItems: 1,
+        items: {
+          type: 'boolean',
+        },
+      },
+      subQuestions: [
+        {
+          anyValuesMatch: 'true',
+          questionIds: ['300007'],
+        },
+      ],
+    },
+    {
+      content: [
+        {
+          description: 'If so, select which ones (multiple possible)',
+          label: 'If so, select which ones (multiple possible)',
+          locale: 'en-US',
+        },
+      ],
+      defaultLocale: 'en-US',
+      description: 'If so, select which ones (multiple possible)',
+      id: '300007',
+      parentQuestionId: '300006',
+      responseSchema: {
+        type: 'array',
+        minItems: 1,
+        maxItems: 1,
+        items: {
+          type: 'string',
+        },
+      },
+      subQuestions: [],
+    },
   ],
 };
