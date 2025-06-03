3b6cb0410dd5b75a45b0f01be3bd43b2775a330c
Hyungho Seo <hyungho.seo@jpmchase.com>
2025-06-03 15:54:00 -0700

    update contact details form title

--- embedded-components/src/core/OnboardingWizardBasic/OnboardingOverviewFlow/flowConfig.ts
+++ embedded-components/src/core/OnboardingWizardBasic/OnboardingOverviewFlow/flowConfig.ts
@@ -151,7 +151,7 @@ const sectionScreens: SectionScreenConfig[] = [
         },
         {
           id: 'contact-details',
-          title: 'Contact details',
+          title: 'Your contact details',
           stepType: 'form',
           description:
             'We need some additional details to confirm your identity.',
