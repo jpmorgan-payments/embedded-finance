8abc703b0e1524951751027f8581a1b86f7a3e2d
Hyungho Seo <hyungho.seo@jpmchase.com>
2025-06-04 14:13:49 -0700

    use 1.0.12-mp spec

--- embedded-components/orval.config.cjs
+++ embedded-components/orval.config.cjs
@@ -28,7 +28,7 @@ module.exports = {
     },
   },
   smbdo: {
-    input: './api-specs/embedded-finance-pub-smbdo-1.0.15.yaml',
+    input: './api-specs/embedded-finance-pub-smbdo-1.0.12-mp.yaml',
     output: {
       mode: 'split',
       target: './src/api/generated/smbdo.ts',
