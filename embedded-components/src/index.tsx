600b34670c5ca812baecffc95f3a3052d854cf7b
Eugene Kontelev <491908+kontel@users.noreply.github.com>
2025-07-23 10:10:24 -0400

    feat: add Accounts component to the component registry and export it

--- embedded-components/src/index.tsx
+++ embedded-components/src/index.tsx
@@ -9,6 +9,7 @@ export { initEBComponentsManager } from './vanilla/EBComponentsManager';
 export * from './core/TransactionsDisplay/TransactionsDisplay';
 export * from './core/MakePayment/MakePayment';
 export * from './core/Recipients';
+export * from './core/Accounts';
 
 export * from './core/OnboardingWizardBasic/OnboardingWizardBasic';
 export type * from './core/OnboardingWizardBasic/utils/types';
