649d5cee91b423b8edf3f0e9c2bf34540b6a58a0
Eugene Kontelev <491908+kontel@users.noreply.github.com>
2025-08-06 15:11:45 -0400

    style: format imports in Recipients story for improved readability

--- embedded-components/src/core/Recipients/stories/Recipients.story.tsx
+++ embedded-components/src/core/Recipients/stories/Recipients.story.tsx
@@ -1,15 +1,18 @@
-import { createMockRecipient, createMockRecipientsResponse, mockActiveRecipients, mockEmptyRecipientsResponse, mockRecipientsResponse } from '@/mocks/recipients.mock';
+import {
+  createMockRecipient,
+  createMockRecipientsResponse,
+  mockActiveRecipients,
+  mockEmptyRecipientsResponse,
+  mockRecipientsResponse,
+} from '@/mocks/recipients.mock';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { http, HttpResponse } from 'msw';
 import { userEvent, within } from '@test-utils';
-
-
 
 import { EBComponentsProvider } from '../../EBComponentsProvider';
 import { MakePayment } from '../../MakePayment';
 import { SELLSENSE_THEME } from '../../themes';
 import { Recipients, RecipientsProps } from '../Recipients';
-
 
 // Wrapper component that follows the same pattern as TransactionsDisplay
 const RecipientsWithProvider = ({
