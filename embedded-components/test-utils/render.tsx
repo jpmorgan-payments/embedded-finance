102d9105b17d90fe1e02c37b0d8efe1670969994
Yevgen Kontelov <491908+kontel@users.noreply.github.com>
2025-12-22 19:06:34 -0500

    fix: add return type to render function for improved type safety

--- embedded-components/test-utils/render.tsx
+++ embedded-components/test-utils/render.tsx
@@ -1,7 +1,7 @@
 import { EBComponentsProvider } from '@/index';
-import { render as testingLibraryRender } from '@testing-library/react';
+import { render as testingLibraryRender, RenderResult } from '@testing-library/react';
 
-export function render(ui: React.ReactNode) {
+export function render(ui: React.ReactNode): RenderResult {
   return testingLibraryRender(<>{ui}</>, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
       <EBComponentsProvider
