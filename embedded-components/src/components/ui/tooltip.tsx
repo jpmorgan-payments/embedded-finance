afd539927dba3c1036bdfdd6a5bde2f8c6a0503d
Seo, Hyungho <hyungho.seo@jpmchase.com>
2026-01-12 15:59:16 -0500

    add eb-component class to tooltip

--- embedded-components/src/components/ui/tooltip.tsx
+++ embedded-components/src/components/ui/tooltip.tsx
@@ -46,7 +46,7 @@ function TooltipContent({
         data-slot="tooltip-content"
         sideOffset={sideOffset}
         className={cn(
-          'eb-z-50 eb-w-fit eb-origin-[--radix-tooltip-content-transform-origin] eb-text-balance eb-rounded-md eb-bg-foreground eb-px-3 eb-py-1.5 eb-text-xs eb-text-background eb-animate-in eb-fade-in-0 eb-zoom-in-95 data-[state=closed]:eb-animate-out data-[state=closed]:eb-fade-out-0 data-[state=closed]:eb-zoom-out-95 data-[side=bottom]:eb-slide-in-from-top-2 data-[side=left]:eb-slide-in-from-right-2 data-[side=right]:eb-slide-in-from-left-2 data-[side=top]:eb-slide-in-from-bottom-2',
+          'eb-component eb-z-50 eb-w-fit eb-origin-[--radix-tooltip-content-transform-origin] eb-text-balance eb-rounded-md eb-bg-foreground eb-px-3 eb-py-1.5 eb-text-xs eb-text-background eb-animate-in eb-fade-in-0 eb-zoom-in-95 data-[state=closed]:eb-animate-out data-[state=closed]:eb-fade-out-0 data-[state=closed]:eb-zoom-out-95 data-[side=bottom]:eb-slide-in-from-top-2 data-[side=left]:eb-slide-in-from-right-2 data-[side=right]:eb-slide-in-from-left-2 data-[side=top]:eb-slide-in-from-bottom-2',
           className
         )}
         {...props}
