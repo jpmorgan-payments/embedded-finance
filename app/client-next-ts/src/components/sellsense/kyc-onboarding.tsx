9b23d938f20e5935386ea4c98ad6e380c40f877e
Hyungho Seo <hyungho.seo@jpmchase.com>
2026-03-16 21:18:17 +0000

    showcase: update to 0.11.28

--- app/client-next-ts/src/components/sellsense/kyc-onboarding.tsx
+++ app/client-next-ts/src/components/sellsense/kyc-onboarding.tsx
@@ -143,6 +143,7 @@ export function KycOnboarding({
         ]}
         userEventsHandler={handleUserEvents}
         docUploadOnlyMode={isOnboardingDocsNeededScenario(clientScenario)}
+        showLinkAccountStep
       />
     </EBComponentsProvider>
   );
